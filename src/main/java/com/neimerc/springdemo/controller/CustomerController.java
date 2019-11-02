18da80e154b7a07f2a82bce270f75e1c244b82e3
Neimerc <Neil_Mercury@yahoo.es>
2019-11-02 06:57:43 +0100

    Inject repository (dao) into controller

--- src/main/java/com/neimerc/springdemo/controller/CustomerController.java
+++ src/main/java/com/neimerc/springdemo/controller/CustomerController.java
@@ -1,16 +1,34 @@
 package com.neimerc.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.neimerc.springdemo.dao.CustomerDAO;
+import com.neimerc.springdemo.entity.Customer;
 
 @Controller
+@RequestMapping("/customer")
 public class CustomerController {
 
-	// Example to confirm that all go well. It will be deleted
-	@GetMapping("/")
-	public String showHome() {
+	// need to inject our customer service
+	@Autowired
+	private CustomerDAO customerDAO;
+	
+	@GetMapping("/list")
+	public String listCustomers(Model theModel) {
 		
-		return "home";
+		// get customers from the service
+		List<Customer> theCustomers = customerDAO.getCustomers();
+				
+		// add the customers to the model
+		theModel.addAttribute("customers", theCustomers);
+		
+		return "list-customers";
 	}
 	
 }
