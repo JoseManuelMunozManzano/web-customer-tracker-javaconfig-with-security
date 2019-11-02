45c3e8bfa24202e97b3d7ba2ba456834458a8ab6
Neimerc <Neil_Mercury@yahoo.es>
2019-11-02 10:59:47 +0100

    Update Customer, creating the link and pre-populate the form

--- src/main/java/com/neimerc/springdemo/dao/CustomerDAOImpl.java
+++ src/main/java/com/neimerc/springdemo/dao/CustomerDAOImpl.java
@@ -43,6 +43,18 @@ public class CustomerDAOImpl implements CustomerDAO {
 		
 		// save/upate the customer ... finally LOL
 		currentSession.saveOrUpdate(theCustomer);
+	}
+	
+	@Override
+	public Customer getCustomer(int theId) {
+
+		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// now retrieve/read from database using the primary key
+		Customer theCustomer = currentSession.get(Customer.class, theId);
+		
+		return theCustomer;
 	}	
 	
 }
