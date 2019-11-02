4e8e69551596076f44f0db2bab64130f9831cd2a
Neimerc <Neil_Mercury@yahoo.es>
2019-11-02 10:29:23 +0100

    Added the new customer to the database

--- src/main/java/com/neimerc/springdemo/dao/CustomerDAO.java
+++ src/main/java/com/neimerc/springdemo/dao/CustomerDAO.java
@@ -7,4 +7,6 @@ import com.neimerc.springdemo.entity.Customer;
 public interface CustomerDAO {
 
 	public List<Customer> getCustomers();
+	
+	public void saveCustomer(Customer theCustomer);
 }
--- src/main/java/com/neimerc/springdemo/service/CustomerService.java
+++ src/main/java/com/neimerc/springdemo/service/CustomerService.java
@@ -7,4 +7,6 @@ import com.neimerc.springdemo.entity.Customer;
 public interface CustomerService {
 
 	public List<Customer> getCustomers();
+	
+	public void saveCustomer(Customer theCustomer);
 }
