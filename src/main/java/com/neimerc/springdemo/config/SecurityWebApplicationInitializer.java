4851042d9610957729de7ffaa56fe0e84a10ba54
Neimerc <Neil_Mercury@yahoo.es>
2019-11-02 11:30:35 +0100

    Added security properties file and update DemoAppConfig. Added new security config and Application Initializer classes.

---
+++ src/main/java/com/neimerc/springdemo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.neimerc.springdemo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer 
+						extends AbstractSecurityWebApplicationInitializer {
+
+}
